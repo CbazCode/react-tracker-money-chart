3f0fc0077f600daeb2fabfe9166c39031bfe0bc7
Sebastian Asis <sasisromero10@gmail.com>
2021-02-19 13:06:59 -0500

    :dart: IMPROVE: Chartjs completed

--- src/ChartCoin.js
+++ src/ChartCoin.js
@@ -1,47 +1,88 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import Chartjs from 'chart.js'   
 import { CoinContext } from './context/CoinContext';
+import './ChartCoin.css';
+import { useParams } from 'react-router-dom';
 
 export const ChartCoin = ({history}) => {
     const chartRef = useRef();
-    const {coins} = useContext(CoinContext);
-    console.log(coins);
+    const {coins} = useContext(CoinContext);;
+    const {id:coinId} = useParams();
+    
+
+    const getCoinById = (coinId) => {
+        const coinGetted = coins?.find((coin) => {
+            return coin.id === coinId
+        })
+        
+        // const {sparkline_in_7d} = encontrado
+        // const {price} = sparkline_in_7d;
+        // console.log(price);
+        return coinGetted;
+    }
+
+    const {name, sparkline_in_7d:{ price }} = getCoinById(coinId)
+    
     // TODO : usar useparams para obter el id
     // TODO : filtar la moneda por el id
     // TODO :obtener los valores para la tabla 
+    const priceFormated = price.map( (prc)=>{
+        return prc.toFixed(4);
+    })
+
+
+
+    
     useEffect(() => {
+        
+        console.log(priceFormated);
         if (chartRef.current && chartRef){
             const chartInstance =   new Chartjs (chartRef.current,{
-                type: 'bar',
+                type: 'line',
                 data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    //Eje X
+                    labels: priceFormated,
                     datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                        label: 'Variability of the currency',
+                        //Eje Y
+                        data: priceFormated,
+                        borderColor: 'orange',
+                        backgroundColor: 'rgba(255,140,0, 0.2)',
                         borderWidth: 1
                     }]
+                    
                 },
                 options: {
+                    legend: {
+                        labels: {
+                          fontColor: 'white'
+                        }
+                      },
                     scales: {
+                        xAxes: [{
+                            mirror: false,
+                            gridLines: {
+                              display: false,
+                            
+                            },
+                            
+                            ticks:{
+                                display: false
+                            }
+                        }],
                         yAxes: [{
+                            gridLines: {
+                                display: false,
+                              
+                            },
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Price ($)",
+                                fontColor: "white"
+                            },
                             ticks: {
-                                beginAtZero: true
+                                beginAtZero: false,
+                                fontColor: "white"
                             }
                         }]
                     }
@@ -51,8 +92,11 @@ export const ChartCoin = ({history}) => {
         
     }, [])
     return (
-        <div>
-            <canvas ref = {chartRef} id = "myChart" width = {250} height = {250} ></canvas>
+        <div className = "chart">
+            <div className = "chart-container">
+                <h2>Follow you currency {name}</h2>
+                <canvas ref = {chartRef} id = "myChart" width = {500} height = {200} ></canvas>
+            </div>
         </div>
     )
 }
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -19,26 +19,29 @@ export const useAxios = (url) => {
               data: null, loading:true, error: null
           })
 
-          axios.get(url)
-            .then( res => {
-                if(isMount.current){
-                    setState(
-                        {
-                            loading: false,
-                            error: null,
-                            data: res.data
-                        }
-                    )
-                }
+          
+              
+        axios.get(url)
+        .then( res => {
+            if(isMount.current){
+                setState(
+                    {
+                        loading: false,
+                        error: null,
+                        data: res.data
+                    }
+                )
+            }
+        })
+        .catch( ()=>{
+            setState({
+                data:null,
+                loading: false,
+                error: 'No se pudo cargar la info'
             })
-            .catch( ()=>{
-                setState({
-                    data:null,
-                    loading: false,
-                    error: 'No se pudo cargar la info'
-                })
+
+        })
     
-            })
     },[url])
 
       return state;
